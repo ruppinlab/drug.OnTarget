9ac91387754b4981604256fe5f96164e06e6a96f
Sanju Sinha <sanju@terpmail.umd.edu>
2022-11-27 00:47:06 -0500

    Structurev0

--- 3.Tools/Validation/A7_Validation_historicCompounds_ChemicalProbes.R
+++
@@ -1,4 +0,0 @@
-# Testing on the change in historic compound annotations;
-setwd('/Users/sinhas8/Project_OffTarget/')
-historic_compounds=read.csv('2.Data/browse_historic_compounds.csv')
-head(historic_compounds$Note.about.actvity, 20)
--- 3.Tools/Validation/A8_Validation_ProbeMiner.R
+++
@@ -1,2 +0,0 @@
-# Using ProbeMiner based score of DrugTargets
-probeminer=read.table('/Users/sinhas8/Downloads/probeminer_datadump_2019-07-01.txt', fill = T)
